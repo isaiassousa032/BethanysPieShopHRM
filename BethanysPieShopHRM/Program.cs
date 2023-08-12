68e8adb7bc7465bbfcad8e0dd6884d59352f2ffb
Isaias Santos <isaiassousa032@gmail.com>
2023-08-12 17:38:51 -0300

    Criação da struct "WorkTask"

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -4,25 +4,23 @@ Console.WriteLine("Creating an Employee");
 Console.WriteLine("--------------------\n");
 
 Employee bethany = new("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25, EmployeeType.Manager);
-Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30, EmployeeType.Research);
 
-Console.WriteLine("\nCreating an Employee");
-Console.WriteLine("--------------------\n");
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30, EmployeeType.Research);
 
 bethany.DisplayEmployeeDetails();
 bethany.PerformWork();
 bethany.PerformWork();
 bethany.PerformWork(5);
 bethany.PerformWork();
-bethany.PerformWork();
-
-int minimumBonus = 100;
-int bonusTax;
-int receivedBonus = bethany.CalculateBonusAndTax(minimumBonus, out bonusTax);
-Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.FirstName} has received a bonus of {receivedBonus}");
 
 double receivedWageBethany = bethany.ReceiveWage(true);
-Console.WriteLine($"Wage paid (message from Program): {receivedWageBethany}");
+Console.WriteLine($"Wage paid (message from Program): {receivedWageBethany}\n");
+
+string bethanyAsJson = bethany.ConvertToJson();
+Console.WriteLine(bethanyAsJson);
+
+Console.WriteLine("\nCreating an Employee");
+Console.WriteLine("--------------------\n");
 
 george.DisplayEmployeeDetails();
 george.PerformWork();
@@ -32,9 +30,17 @@ george.PerformWork();
 george.PerformWork(8);
 
 var receivedWageGeorge = george.ReceiveWage(true);
+
 Console.WriteLine($"Wage paid (message from Program): {receivedWageGeorge}\n");
 
-string bethanyAsJson = bethany.ConvertToJson();
-Console.WriteLine(bethanyAsJson);
+WorkTask task;
+task.description = "Bake delicious pies";
+task.hours = 3;
+task.PerformWorkTask();
+
+string georgeAsJson = bethany.ConvertToJson();
+Console.WriteLine(georgeAsJson);
+
+
 
 
