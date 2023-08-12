e985e987bfaca9165785c27db76feb5f37a3837a
Isaias Santos <isaiassousa032@gmail.com>
2023-08-12 13:26:26 -0300

    criação do construtor e Reparação das Propriedades. (estavam começando com letras minúsculas)

--- BethanysPieShopHRM/Employee.cs
+++ BethanysPieShopHRM/Employee.cs
@@ -8,15 +8,24 @@ namespace BethanysPieShopHRM
 {
     internal class Employee
     {
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string email { get; set; }
+        public Employee(string first, string last, string em, DateTime bd, double rate)
+        {
+            FirstName = first;
+            LastName = last;
+            Email = em;
+            Birthday = bd;
+            HourlyRate = rate;
+        }
 
-        public int numberOfHoursWorked;
-        public double wage { get; set; }
-        public double hourlyRate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
 
-        public DateTime birthday { get; set; }
+        public int NumberOfHoursWorked;
+        public double Wage { get; set; }
+        public double HourlyRate { get; set; }
+
+        public DateTime Birthday { get; set; }
 
         const int minimalHoursWorkedUnit = 1;
 
@@ -27,25 +36,25 @@ namespace BethanysPieShopHRM
 
         public void PerformWork(int numberOfHours)
         {
-            numberOfHoursWorked += numberOfHours;
+            NumberOfHoursWorked += numberOfHours;
 
-            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)");
+            Console.WriteLine($"{FirstName} {LastName} has worked for {NumberOfHoursWorked} hour(s)");
         }
 
         public double ReceiveWage(bool reserHours = true)
         {
-            wage = numberOfHoursWorked * hourlyRate;
+            Wage = NumberOfHoursWorked * HourlyRate;
 
-            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hours(s) of work");
+            Console.WriteLine($"{FirstName} {LastName} has received a wage of {Wage} for {NumberOfHoursWorked} hours(s) of work");
 
             if (reserHours)
-                numberOfHoursWorked = 0;
-            return wage;
+                NumberOfHoursWorked = 0;
+            return Wage;
         }
 
         public void DisplayEmployeeDetails()
         {
-            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail:\t\t{email}\nBirthday: \t{birthday.ToShortDateString()}\n");
+            Console.WriteLine($"\nFirst name: \t{FirstName}\nLast name: \t{LastName}\nEmail:\t\t{Email}\nBirthday: \t{Birthday.ToShortDateString()}\n");
         }
     }
 }
