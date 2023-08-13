c6f0edf0795cdf83ba266227ea49fdec74920012
Isaias Santos <isaiassousa032@gmail.com>
2023-08-12 21:07:47 -0300

    Utilizando Arrays

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -11,8 +11,8 @@ Console.WriteLine("--------------------\n");
 
 Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), null, EmployeeType.Research);
 
-Employee mysteryEmployee = null;
-mysteryEmployee.DisplayEmployeeDetails();
+//Employee mysteryEmployee = null;
+//mysteryEmployee.DisplayEmployeeDetails();
 
 #region First run Bethany
 
@@ -62,5 +62,30 @@ george.DisplayEmployeeDetails();
 Employee.DisplayTaxRate();
 
 bethany.CalculateWage();
+
+int[] sampleArray1 = new int[5];
+
+int[] sampleArray2 = new int[] { 1, 2, 3, 4, 5 };
+int[] sampleArray3 = new int[5] { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("How many employees IDs do you want to register?");
+
+int length = int.Parse(Console.ReadLine());
+
+int[] employeeIds = new int[length];
+
+var testId = employeeIds[0];
+
+for (int i = 0; i < length; i++)
+{
+    Console.Write("Enter the employee ID:");
+    int id = int.Parse(Console.ReadLine());
+    employeeIds[i] = id;
+}
+
+for (int i = 0; i < employeeIds.Length; i++)
+{
+    Console.WriteLine($"ID {i + 1}: \t{employeeIds[i]}");
+}
 
 
